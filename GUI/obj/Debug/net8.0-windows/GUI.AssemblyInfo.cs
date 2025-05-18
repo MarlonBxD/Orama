9bdedd4262e9fee1f2c34ab5e07883a95de1092b
Marlon <marlonbuelvas314@gmail.com>
2025-05-17 23:15:14 -0500

    Menu Principal

--- GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9ab7ce384c59628b8ac4b915c7446a1864b21d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b5eb0135e76e8d7a506243dc9566091bddcf7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
